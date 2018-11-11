806ad9e31e26128e1a65dccce4a236dc81553aef
gitname <gitname>
2018-11-11 20:07:30 +0800

    dev

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+        System.out.println("dev");
  System.out.println("dev");
     }
 }
